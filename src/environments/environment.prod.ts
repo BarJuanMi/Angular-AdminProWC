dafab465d8276efc9f1c06e6ba48da4019e9d5ef
BarJuanMi <barretodiaz@gmail.com>
2022-03-28 19:08:03 -0500

    Cambio para deploy en digital ocean

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  base_url: 'http://164.92.67.11:3001/api',
-  url_load_pdf_pazysalvo: 'http://164.92.67.11:3001/api/files/uploadspdf/obtenerpdf/pazysalvos/',
+  base_url: 'https://back-end-pro-wc-ehxwh.ondigitalocean.app/api',
+  url_load_pdf_pazysalvo: 'https://back-end-pro-wc-ehxwh.ondigitalocean.app/api/files/uploadspdf/obtenerpdf/pazysalvos/',
   url_load_pdf_hojas_vida: '/files/uploadspdf/obtenerpdf/hojasvida/',
   url_load_pdf_resp_psico: '/files/uploadspdf/obtenerpdf/respsicologico/',
 };
