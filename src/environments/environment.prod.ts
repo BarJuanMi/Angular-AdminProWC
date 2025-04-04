057025fd11f32e81ef89f97417d9e69863d0c82f
Juan Miguel Barreto Diaz <ecf1398b@claro.com.co>
2025-04-04 10:17:54 -0500

    Cambio de apuntamiento

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'http://18.225.117.139:3001/api',
+  base_url: 'http://44.203.254.126:3001/api',
   url_load_pdf_pazysalvo: '/files/uploadspdf/obtenerpdf/pazysalvos/',
   url_load_pdf_hojas_vida: '/files/uploadspdf/obtenerpdf/hojasvida/',
   url_load_pdf_ausentismo: '/files/uploadspdf/obtenerpdf/ausentismos/',
